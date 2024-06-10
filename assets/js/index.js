58a53ac9fbaed9cd950fc14b0f0bdb57fed9955f
Maria Fernanda <160313240+nandamyrita@users.noreply.github.com>
2024-06-10 11:14:46 -0300

    commitnow

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,3 +2,11 @@ document.querySelector('.text-form').addEventListener('submit', function (event)
     event.preventDefault();
     alert('Sua mensagem foi enviada com sucesso!');
   });
+
+  function scrollToSection (id) {
+    document.getElementById(id).scrollIntoView({ behavior:'smooth'});
+  }
+
+  function navigateTo(page){
+    window.location.href = page;
+  }
